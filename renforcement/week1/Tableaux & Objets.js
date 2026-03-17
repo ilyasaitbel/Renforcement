55d8420a9a4bb7c6ae823e693e61d414fa3934de
ilyasaitbel <ilyasilya084@gmail.com>
2026-03-17 14:28:55 +0000

    .\renforcement\

---
+++ renforcement/week1/Tableaux & Objets.js
@@ -0,0 +1,53 @@
+// 1. Crée un tableau "fruits" avec 3 fruits
+let fruits = ['banane','pomme','orange'];
+// 2. Affiche le deuxième élément
+console.log(fruits[1]);
+
+let couleurs = ["rouge","blue","vert"]
+
+couleurs.push("violet")
+
+console.log(couleurs.length)
+
+let pesonne = {
+    nom:"ilyas",
+    age:19,
+    ville:'marrakech'
+}
+console.log(pesonne.nom)
+
+let animaux = ["chat","chien","lapin","tortue"];
+
+for (let i = 0 ; i<animaux.length;i++){
+    console.log(animaux[i]);
+}
+let coureses = [
+    {nom:'JS',prix:28},
+    {nom:'oop',prix:7},
+    {nom:'python',prix:10}
+]
+let sum = 0;
+for (let i = 0 ; i<coureses.length ; i++)
+{
+    sum = sum + coureses[i].prix
+}
+console.log(sum);
+
+
+coureses[0].quantite = 2;
+coureses[1].quantite = 3;
+coureses[2].quantite = 4;
+
+let somme = 0;
+for (let i = 0 ; i<coureses.length ; i++)
+{
+    somme = somme + coureses[i].prix * coureses[i].quantite
+}
+console.log(somme)
+
+let produit = {
+    nom:"laptop",
+    prix:30000,
+    categorie:"it"
+}
+console.log('Le produit ' + produit.nom + ' coute ' + produit.prix + ' dh (categorie: ' + produit.categorie + ')');
