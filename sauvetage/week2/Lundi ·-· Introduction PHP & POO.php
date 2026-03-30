96684d5480769490715a6aaa46d7b67061e64240
ilyasaitbel <ilyasilya084@gmail.com>
2026-03-30 10:05:43 +0100

    Organisation

---
+++ sauvetage/week2/Lundi ·-· Introduction PHP & POO.php
@@ -0,0 +1,14 @@
+<?php
+
+$prenom = 'ilyas';
+$age = 19;
+
+echo "bonjour je m'appelle $prenom et j'ai $age ans.\n";
+
+if ($age >= 18) {
+    echo 'Majeur';
+}else{
+    echo 'Mineur';
+}
+
+$status = 
